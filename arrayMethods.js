708eec77fc359c8e3d283d117305e8d542c88305
Michael Hardcastle <maaicle@gmail.com>
2021-10-21 08:15:35 -0600

    completed arrayMethods.js

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,8 +18,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(element => element % 2 === 0);
 
+// console.log(evenNumbers);
 
 
 ////////// PROBLEM 2 //////////
@@ -39,9 +40,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
-
+const postTaxPrices = prices.map(element => element * (1 + .07));
+// console.log(postTaxPrices);
 
 ////////// PROBLEM 3 //////////
 
@@ -57,7 +57,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce((runningTotal, curElement) => runningTotal + curElement);
+// console.log(totalPopulation);
 
 
 
@@ -82,7 +83,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(element => element.CP > 200);
+// console.log(myStrongest);
 
 
 
@@ -96,11 +98,13 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, 
+  hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
 // CODE HERE
-
+const orderTotals = orders.map(element => element.price * (1 + element.tax));
+// console.log(orderTotals);
 
 
 ////////// PROBLEM 6 //////////
@@ -120,3 +124,5 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const purchaseTotals = purchases.map(object => object.price).reduce((acc, curr) => acc + curr);
+console.log(purchaseTotals);
